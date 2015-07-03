5464e08210eaf1acd57419988ac5d4b43276ca82
Brian Hong <sungjinhong@devsisters.com>
2015-07-03 13:33:37 +0900

    Refactor actor_test

--- actor_test.go
+++ actor_test.go
@@ -4,95 +4,105 @@ import (
 	"testing"
 )
 
-type A struct {
+type TestActor struct {
 	Actor
-	x  int
-	y  int
-	in chan GetXRequest
+	t *testing.T
+	x int
+	y int
+
+	// For BenchmarkChannel test
+	in chan AddXRequest
 }
 
-type GetXRequest struct {
+// For BenchmarkChannel test
+type AddXRequest struct {
 	x   int
-	out chan GetXResponse
+	out chan AddXResponse
 }
 
-type GetXResponse struct {
+// For BenchmarkChannel test
+type AddXResponse struct {
 	x   int
 	err interface{}
 }
 
-func (a *A) GoX(x int) int {
-	out := make(chan GetXResponse)
-	a.in <- GetXRequest{x, out}
+// For BenchmarkChannel test
+func (a *TestActor) GoAddX(x int) int {
+	out := make(chan AddXResponse)
+	a.in <- AddXRequest{x, out}
 	return (<-out).x
 }
 
-func (a *A) ProcessGetX() {
+// For BenchmarkChannel test
+func (a *TestActor) ProcessAddX() {
 	for {
-		request := <-a.in
-		request.out <- GetXResponse{a.GetX(request.x), nil}
+		request, ok := <-a.in
+		if !ok {
+			break
+		}
+		request.out <- AddXResponse{a.AddX(request.x), nil}
 	}
 }
 
-func (a *A) GetX(x int) int {
-	return a.x + x
-}
-
-func (a *A) DoPanic() int {
+func (a *TestActor) DoPanic() int {
 	panic(a.y)
 }
 
-func (a *A) Terminate(errReason error) {
+func (a *TestActor) Terminate(errReason error) {
+	panicErr, ok := errReason.(*PanicError)
+	if !ok {
+		a.t.Errorf("Expected PanicError but got %v\n", errReason)
+	}
+	if panicErr.PanicErr.(int) != a.y {
+		a.t.Errorf("Expected PanicError value to be %v but got %v\n", a.y, panicErr.PanicErr)
+	}
 }
 
-func TestGetX(t *testing.T) {
-	a := A{Actor{}, 2, 3, nil}
+func (a *TestActor) AddX(x int) int {
+	return a.x + x
+}
+
+func BenchmarkChannel(b *testing.B) {
+	a := TestActor{Actor{}, nil, 5, 10, make(chan AddXRequest)}
+	go a.ProcessAddX()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		a.GoAddX(3)
+	}
+	close(a.in)
+}
+
+func BenchmarkActor(b *testing.B) {
+	a := TestActor{Actor{}, nil, 5, 10, nil}
+	a.startMessageLoop(&a)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		a.call((*TestActor).AddX, 3)
+	}
+}
+
+func TestAddX(t *testing.T) {
+	a := TestActor{Actor{}, t, 2, 3, nil}
 	a.startMessageLoop(&a)
 
-	r, _ := a.call((*A).GetX, 4)
+	r, err := a.call((*TestActor).AddX, 4)
+	if err != nil {
+		t.Errorf("Expected no error, got %v\n", err)
+	}
+	if len(r) != 1 {
+		t.Errorf("Expected 1 return values, got %d results\n", len(r))
+	}
 	if x := r[0].(int); x != 6 {
 		t.Errorf("Expected x = %v, actual %v\n", 6, x)
 	}
 }
 
 func TestPanic(t *testing.T) {
-	a := A{Actor{}, 2, 3, nil}
+	a := TestActor{Actor{}, t, 2, 3, nil}
 	a.startMessageLoop(&a)
 
-	defer func() {
-		if e := recover(); e != nil {
-			if response, ok := e.(Response); ok {
-				if panicCause, ok2 := response.PanicCause().(int); !ok2 || panicCause != 3 {
-					t.Errorf("Expected to receive panic response == 3, actual %v\n", panicCause)
-				}
-			} else {
-				t.Errorf("Expected panic(Response), actual %v\n", e)
-			}
-		}
-	}()
-
-	_, err := a.call((*A).DoPanic)
-	if err == nil {
-		t.Errorf("Expected panic, no error returned")
-	}
-}
-
-func BenchmarkActor(b *testing.B) {
-	b.StopTimer()
-	a := A{Actor{}, 5, 10, nil}
-	a.startMessageLoop(&a)
-	b.StartTimer()
-	for i := 0; i < b.N; i++ {
-		a.call((*A).GetX, 3)
-	}
-}
-
-func BenchmarkChannel(b *testing.B) {
-	b.StopTimer()
-	a := A{Actor{}, 5, 10, make(chan GetXRequest)}
-	go a.ProcessGetX()
-	b.StartTimer()
-	for i := 0; i < b.N; i++ {
-		a.GoX(3)
+	_, err := a.call((*TestActor).DoPanic)
+	if err != ErrActorDied {
+		t.Errorf("Expected ErrActorDied error, instead got %v\n", err)
 	}
 }
